48baabac4fc73ae0ecd6bfe4b8e0728281a8f0ae
Rahul Govindaraju <rahul@gmail.com>
2025-12-30 02:03:14 +0530

    testing backend

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file
 from analyzer import analyze_url
 from report import generate_pdf
+from io import BytesIO
 
 app = Flask(__name__)
 
-last_result = None
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/scan", methods=["POST"])
 def scan():
-    global last_result
     data = request.json
-    result = analyze_url(data["url"])
-    last_result = result
+    url = data.get("url")
+
+    if not url:
+        return jsonify({"error": "No URL provided"}), 400
+
+    result = analyze_url(url)
     return jsonify(result)
 
-from flask import send_file
-
-@app.route("/download")
+@app.route("/download", methods=["POST"])
 def download():
-    global last_result
-    if not last_result:
-        return "No report available", 400
+    result = request.json
+    if not result:
+        return "No scan data", 400
 
-    pdf_buffer = generate_pdf(last_result)
+    pdf_buffer = generate_pdf(result)
+
     return send_file(
         pdf_buffer,
         mimetype="application/pdf",
         as_attachment=True,
         download_name="phishshield_report.pdf"
     )
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
